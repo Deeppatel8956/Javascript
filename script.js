af7ae88ca32f2ea5194c9a9148f227c96f0d8736
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-09-12 18:30:32 +0530

    Break and continue

---
+++ script.js
@@ -0,0 +1,22 @@
+for (let index = 0; index <=10; index++) {
+    const element = index;
+    console.log(`print value ${element}`);
+    if (element==5) {
+        // console.log(`break use ${element}`);
+        console.log(`continue use ${element}`);
+        // break;
+        continue;
+    }
+    
+}
+// let a=0
+// while ( a<10) {
+   
+//    a++
+//    const element=a
+//    if (element==5) {
+//     console.log(`print ${element}`);
+//     continue;
+//    }
+// }
+// console.log(a) ;
