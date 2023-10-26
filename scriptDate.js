52ad9ae1950eb5952adf0446ecf32be21ff3d493
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-10-26 14:50:25 +0530

    Date in js

---
+++ scriptDate.js
@@ -0,0 +1,8 @@
+//---------------Date and Time-------------------
+let mydate=new Date();
+// console.log(mydate.toString())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toTimeString())
+console.log(Date.now());
