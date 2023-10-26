ec44d60a3e22a4cd12dc99a27bd37421f0bfe615
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-10-26 14:49:31 +0530

    Array method

---
+++ Array.js
@@ -0,0 +1,21 @@
+const myArray=[10,20,30,9,70];
+// console.log(myArray[0])
+console.log(myArray)
+// console.log(myArray.includes(20))
+//return boolean value
+
+let demo=[100,200];
+
+console.log(myArray.push(demo))
+// console.log(myArray.pop())
+// console.log(myArray.unshift(40));
+// console.log(myArray.shift());
+// let test=myArray.splice(0,2,23,56,89);
+// console.log(test)
+// console.log(myArray.slice(0,2))
+let name="dog";
+console.log(Array.from(name))
+let test1=["hello"];
+let test2=["Aus"];
+let test3=test1.concat(test2);
+console.log(test3.join("-"));
