0674905a526579eec33e23de136d2a0a9b4f271e
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-02-15 18:54:51 +0530

    JavaScript practice

---
+++ 01w3school.js
@@ -0,0 +1,40 @@
+document.write(5+6)
+document.getElementById("god").innerHTML=40+70;
+console.log("hello")
+// window.alert(7+5)
+// varable
+// var
+// let
+// const
+
+a=12
+console.log(typeof a)
+let b="hell"
+console.log(typeof b)
+
+let x,y;
+x=10
+y=20
+document.getElementById('hell').innerHTML=(x+y)*20;
+
+
+//string expression
+document.getElementById('demoed').innerHTML="Jhon"+""+"Doe" 
+
+
+var z, w;
+z = 5 + 6;
+w = z * 10;
+document.getElementById("demoes").innerHTML = w;
+
+// // multiline statement
+// let a,b1,c
+// a=1,b=5,c=a+b1
+// console.log(c)
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
+
+
